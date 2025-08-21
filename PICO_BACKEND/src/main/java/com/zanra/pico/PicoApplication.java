b3b4979c3e10163c3cf6c6317566bc3db5c6c340
zanra2401 <grubgrex@gmail.com>
2025-08-21 21:44:33 +0700

    first commit

---
+++ PICO_BACKEND/src/main/java/com/zanra/pico/PicoApplication.java
@@ -0,0 +1,13 @@
+package com.zanra.pico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicoApplication.class, args);
+	}
+
+}
---
+++ PICO_BACKEND/src/test/java/com/zanra/pico/PicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zanra.pico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
